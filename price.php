4d5e3568ad7f7f8368d1b3d43a247285a5151754
francis974123 <114352583+francis974123@users.noreply.github.com>
2022-09-25 21:32:12 +0530

    Add files via upload

---
+++ price.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="price.css">
+    <title>Pricing Table</title>
+</head>
+<body>
+ 
+
+      <div class="cards">
+
+        <!-- Starter Plan -->
+          <div class="card" id="card-1">
+            <h1 class="card-title">Standard</h1>
+            <h2 class="card-price" id="starter-price">  ₹666 <span> / month</span> </h2>
+            <ul class="card-plan">
+               
+                <li> Free Fitness Training</li>
+                <li> Free WiFi</li>
+                <li>Use of Total Body Enhancement</li>
+                <li>50% Off Select Drinks</li>
+<!--                 <li>Unlimted branded thems</li> -->
+                <li>Email Support</li>
+            </ul>
+<!--             <button type="button" class="card-btn"> Choose Plan</button> -->
+          </div>
+
+          <!-- Pro Plan -->
+          <div class="card active" id="card-2">
+            <h1 class="card-title">Pro</h1>
+            <h2 class="card-price" id="pro-price"> ₹999 <span> / month</span> </h2>
+          
+
+          
+            <ul class="card-plan">
+              
+                <li> Unlimited Access to Home Club  </li>
+                <li> Bring a Guest Anytime</li>
+                <li>Use of HydroMassage</li>
+                <li>Free aerobic classes up to 10 in a month</li>
+                <li>Expert Nutrition Advice</li>
+            </ul>
+
+          </div>
+
+          <!-- Enterpise Card -->
+          <div class="card" id="card-3">
+            <h1 class="card-title">Offer for couple's</h1>
+<br>
+             <h2 class="card-price" id="pro-price"> ₹1599 <span> / month</span> </h2>
+            <ul class="card-plan">
+              <li> Premium account and services</li>
+                <li> Free 1 month personal training</li>
+                <li> free session of premium crossfit course</li>
+                <li> premium gym coupon</li>
+                <li> personal coach</li>
+
+                
+            </ul>
+           
+          </div>
+      </div>
+    </div>
+    <script src="price.js"></script>
+    
+</body>
+</html>
